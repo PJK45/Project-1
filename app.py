2076f006b11eb7a9d751c460c9981e2eeac340a7
PJK45 <pjakkula45@gmail.com>
2026-09-04 16:49:01 -0500

    intial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/greet", methods=["POST"])
+def greet():
+    name = request.form.get("name")
+
+    if not name:
+        name = "Guest"
+
+    return render_template("index.html", message=f"Hello, {name}!")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
